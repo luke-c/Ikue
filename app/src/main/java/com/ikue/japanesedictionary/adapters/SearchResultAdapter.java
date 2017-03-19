fa1c7c760ae815eed0853297ce4f0abbacb40b7a
Luke Casey <lukecasey94@gmail.com>
2017-03-19 15:56:38 +0000

    Remove invisible favourite button from list item

--- app/src/main/java/com/ikue/japanesedictionary/adapters/SearchResultAdapter.java
+++ app/src/main/java/com/ikue/japanesedictionary/adapters/SearchResultAdapter.java
@@ -2,13 +2,11 @@ package com.ikue.japanesedictionary.adapters;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.ikue.japanesedictionary.R;
@@ -33,7 +31,6 @@ public class SearchResultAdapter extends RecyclerView.Adapter<SearchResultAdapte
         public TextView primaryTextView;
         public TextView primaryTextView2;
         public TextView secondaryTextView;
-        public ImageView favouriteButton;
 
         // We also create a constructor that accepts the entire item row
         // and does the view lookups to find each subview
@@ -45,7 +42,6 @@ public class SearchResultAdapter extends RecyclerView.Adapter<SearchResultAdapte
             primaryTextView = (TextView) itemView.findViewById(R.id.list_primary);
             primaryTextView2 = (TextView) itemView.findViewById(R.id.list_primary2);
             secondaryTextView = (TextView) itemView.findViewById(R.id.list_secondary);
-            favouriteButton = (ImageView) itemView.findViewById(R.id.favourite_button);
         }
     }
 
@@ -80,8 +76,7 @@ public class SearchResultAdapter extends RecyclerView.Adapter<SearchResultAdapte
         View view = inflater.inflate(R.layout.search_list_item, parent, false);
 
         // Return a new holder instance
-        ViewHolder viewHolder = new ViewHolder(view);
-        return viewHolder;
+        return new ViewHolder(view);
     }
 
     // Involves populating data into the item through holder
@@ -119,14 +114,6 @@ public class SearchResultAdapter extends RecyclerView.Adapter<SearchResultAdapte
         primaryText2.setText(item.getReadingElementValue());
 
         secondaryText.setText(TextUtils.join(", ", item.getGlossValue()));
-
-        final ImageView favouriteButton = viewHolder.favouriteButton;
-        favouriteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                favouriteButton.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.ic_star_black));
-            }
-        });
     }
 
     // Returns the total count of items in the list
