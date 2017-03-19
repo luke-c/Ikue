7ab90caf43983d12fef6d58eccae257551315ff0
Luke Casey <lukecasey94@gmail.com>
2017-03-19 18:03:44 +0000

    Add terms of use to about page

--- app/src/main/java/com/ikue/japanesedictionary/activities/AboutActivity.java
+++ app/src/main/java/com/ikue/japanesedictionary/activities/AboutActivity.java
@@ -10,6 +10,7 @@ import com.ikue.japanesedictionary.R;
 
 import de.psdev.licensesdialog.LicensesDialog;
 import de.psdev.licensesdialog.licenses.ApacheSoftwareLicense20;
+import de.psdev.licensesdialog.licenses.GnuGeneralPublicLicense30;
 import de.psdev.licensesdialog.licenses.MITLicense;
 import de.psdev.licensesdialog.model.Notice;
 import de.psdev.licensesdialog.model.Notices;
@@ -30,15 +31,38 @@ public class AboutActivity extends AppCompatActivity {
                 .isRTL(false)
                 .setImage(R.mipmap.ic_launcher)
                 .setDescription(getResources().getString(R.string.about_activity_description))
-                .addItem(new Element().setTitle(BuildConfig.VERSION_NAME))
+                .addItem(new Element().setTitle("Version: " + BuildConfig.VERSION_NAME))
                 .addGroup(getResources().getString(R.string.about_activity_connect))
                 .addEmail("lukecasey94+ikue@gmail.com")
                 .addTwitter("lukecasey94")
-                .addGitHub("luke-c")
+                .addGitHub("luke-c/Ikue")
                 .addItem(getLicensesElement())
+                .addItem(getTermsOfUseElement())
                 .create();
 
         setContentView(aboutPage);
+    }
+
+    private Element getTermsOfUseElement() {
+        Element element = new Element();
+        element.setTitle(getResources().getString(R.string.about_activity_terms_of_use));
+        element.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Notice notice = new Notice("Ikue",
+                        "https://github.com/luke-c/Ikue",
+                        "Copyright (C) 2017 Luke Casey",
+                        new GnuGeneralPublicLicense30());
+
+                new LicensesDialog.Builder(AboutActivity.this)
+                        .setTitle(getResources().getString(R.string.about_activity_terms_of_use))
+                        .setNotices(notice)
+                        .setShowFullLicenseText(true)
+                        .build()
+                        .show();
+            }
+        });
+        return element;
     }
 
     private Element getLicensesElement() {
@@ -71,6 +95,13 @@ public class AboutActivity extends AppCompatActivity {
                         "https://github.com/google/flexbox-layout",
                         "",
                         new ApacheSoftwareLicense20()));
+
+                notices.addNotice(new Notice("WanaKanaJava",
+                        "https://github.com/MasterKale/WanaKanaJava",
+                        "The MIT License (MIT)\n" +
+                                "\n" +
+                                "Copyright (c) 2013 Matthew Miller\n",
+                        new MITLicense()));
 
                 new LicensesDialog.Builder(AboutActivity.this)
                         .setTitle(getResources().getString(R.string.about_activity_licenses))
