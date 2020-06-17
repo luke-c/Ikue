d572427a2f5342e553cd1151c49da91c9b678db4
Luke Casey <lukecasey94@gmail.com>
2020-06-17 08:48:20 +0100

    Add bottom navigation

--- app/src/main/java/com/ikue/japanesedictionary/MainActivity.kt
+++ app/src/main/java/com/ikue/japanesedictionary/MainActivity.kt
@@ -1,7 +1,25 @@
 package com.ikue.japanesedictionary
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.ikue.japanesedictionary.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController =
+            (supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment).navController
+        binding.bottomNav.setupWithNavController(navController)
+    }
 }
+
+
